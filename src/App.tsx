fc3b87e5142804ab4f4aac0e6bce339338017a3c
Sarun Maharjan <sarunmrzn@gmail.com>
2021-01-15 01:42:13 +0545

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,304 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
 
-function App() {
+const App = () => {
+  const {
+    name,
+    role,
+    phone,
+    email,
+    website,
+    location,
+    summary,
+    skills,
+    passions,
+    experience,
+    education,
+  } = data;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ margin: "32px", fontFamily: "sans-serif" }}>
+      <div>
+        <div
+          style={{
+            textTransform: "uppercase",
+            fontSize: "2.4rem",
+            fontWeight: "bold",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {name}
+        </div>
+        <div
+          style={{
+            textTransform: "uppercase",
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+            color: "blue",
+          }}
+        >
+          {role}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "60%",
+            flexFlow: "wrap",
+          }}
+        >
+          <div style={{ fontSize: "0.8rem", fontWeight: "bold" }}>{phone}</div>
+          <div style={{ fontSize: "0.8rem", fontWeight: "bold" }}>{email}</div>
+          <div style={{ fontSize: "0.8rem", fontWeight: "bold" }}>
+            {website}
+          </div>
+          <div style={{ fontSize: "0.8rem", fontWeight: "bold" }}>
+            {location}
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "1rem",
+        }}
+      >
+        <div style={{ width: "65%" }}>
+          {/* Summary */}
+          <div>
+            <div
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                width: "100%",
+                borderBottom: "5px solid black",
+              }}
+            >
+              SUMMARY
+            </div>
+            <div style={{ margin: ".5rem 0" }}>
+              <div>{summary[0]}</div>
+              <br />
+              <div>{summary[1]}</div>
+            </div>
+          </div>
+
+          {/* Passion */}
+          <div>
+            <div
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                width: "100%",
+                borderBottom: "5px solid black",
+              }}
+            >
+              PASSIONS
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                flexFlow: "wrap",
+                margin: ".5rem 0",
+                padding: "0 3rem",
+              }}
+            >
+              {passions.map((item) => (
+                <div>{item}</div>
+              ))}
+            </div>
+          </div>
+          {/* Experience */}
+          <div>
+            <div
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                width: "100%",
+                borderBottom: "5px solid black",
+              }}
+            >
+              EXPERIENCE
+            </div>
+            <div style={{ margin: ".5rem 0" }}>
+              {experience?.map(
+                ({
+                  company,
+                  role,
+                  location,
+                  startDate,
+                  endDate,
+                  mainDetails,
+                  details,
+                }) => {
+                  return (
+                    <div>
+                      <div
+                        style={{
+                          fontSize: "1.5rem",
+                          color: "blue",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {company}
+                      </div>
+                      <div
+                        style={{
+                          fontSize: "1.2rem",
+                          margin: "0.5rem 0",
+                        }}
+                      >
+                        {role}
+                      </div>
+
+                      <div style={{ display: "flex" }}>
+                        <div
+                          style={{ paddingRight: "1rem" }}
+                        >{`${startDate} - ${endDate}`}</div>
+                        <div>{location}</div>
+                      </div>
+                      <div>{mainDetails}</div>
+                      <ul style={{ margin: 0 }}>
+                        {details?.map((item) => {
+                          return <li>{item} </li>;
+                        })}
+                      </ul>
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          </div>
+          {/* Education */}
+          <div>
+            <div
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                width: "100%",
+                borderBottom: "5px solid black",
+              }}
+            >
+              EDUCATION
+            </div>
+            <div style={{ margin: ".5rem 0" }}>
+              {education?.map(({ name, startDate, endDate, subject }) => {
+                return (
+                  <div style={{ margin: ".5rem 0" }}>
+                    <div
+                      style={{
+                        fontSize: "1.5rem",
+                        color: "blue",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {name}
+                    </div>
+                    <div style={{ fontSize: "1.2rem" }}>{subject}</div>
+                    <div>{`${startDate} - ${endDate}`}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div style={{ width: "30%" }}>
+          <div>
+            <div
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                width: "100%",
+                borderBottom: "5px solid black",
+              }}
+            >
+              SKILLS
+            </div>
+            <div style={{ margin: "0.5rem 0" }}>
+              <div style={{ color: "blue", fontSize: "1.5rem" }}>Front End</div>
+              <div
+                style={{
+                  display: "flex",
+                  flexFlow: "wrap",
+                  margin: "1rem 0",
+                  lineHeight: "2rem",
+                }}
+              >
+                {skills.frontEnd?.map((ski) => (
+                  <div
+                    style={{
+                      backgroundColor: "blue",
+                      borderRadius: "20px",
+                      color: "white",
+                      textAlign: "center",
+                      padding: "0 0.5rem",
+                      margin: "0.5rem",
+                    }}
+                  >
+                    {ski}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div style={{ margin: "0.5rem 0" }}>
+              <div style={{ color: "blue", fontSize: "1.5rem" }}>Back End</div>
+              <div
+                style={{
+                  display: "flex",
+                  flexFlow: "wrap",
+                  margin: "1rem 0",
+                  lineHeight: "2rem",
+                }}
+              >
+                {skills.backEnd?.map((ski) => (
+                  <div
+                    style={{
+                      backgroundColor: "blue",
+                      borderRadius: "20px",
+                      color: "white",
+                      textAlign: "center",
+                      padding: "0 0.5rem",
+                      margin: "0.5rem",
+                    }}
+                  >
+                    {ski}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div style={{ margin: "0.5rem 0" }}>
+              <div style={{ color: "blue", fontSize: "1.5rem" }}>Tools</div>
+              <div
+                style={{
+                  display: "flex",
+                  flexFlow: "wrap",
+                  margin: "1rem 0",
+                  lineHeight: "2rem",
+                }}
+              >
+                {skills.tools?.map((ski) => (
+                  <div
+                    style={{
+                      backgroundColor: "blue",
+                      borderRadius: "20px",
+                      color: "white",
+                      textAlign: "center",
+                      padding: "0 0.5rem",
+                      margin: "0.5rem",
+                    }}
+                  >
+                    {ski}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.ts
@@ -0,0 +1,69 @@
+export default {
+  name: "Sarun Maharjan",
+  role: "Full Stack Web Developer",
+  phone: "+9779813821815",
+  email: "sarunmrzn@gmail.com",
+  website: "sarunmaharjan.com.np",
+  location: "Kathmandu, Nepal",
+  summary: {
+    0: "I am an enthusiastic, self-motivated, reliable, responsible and hard working person who likes to solve problems. I am a mature team worker and adaptable to all challenging situations. I am able to work well under pressure and adhere to strict deadlines",
+    1: "I merge a passion for usability and user experience with technical knowledge to create cool digital experiences. My repertoire includes programming languages and tools suchas ReactJS, Node.js, Figma and many more libraries and frameworks",
+  },
+  skills: {
+    frontEnd: [
+      "HTML5",
+      "CSS3",
+      "Javascript",
+      "TypeScript",
+      "ReactJS",
+      "React Native",
+      "Gatsby",
+    ],
+    backEnd: ["Node.js", "ExpressJS", "PostgresSQL", "MongoDB"],
+    tools: [
+      "Git",
+      "AdobeXD",
+      "Figma",
+      "Postman",
+      "Visual Studio Code",
+      "npm/yarn",
+      "Jira",
+    ],
+  },
+  passions: ["Gaming", "Music", "Movies"],
+  experience: [
+    {
+      company: "Okhati",
+      location: "Kathmandu, Nepal",
+      role: "Full Stack Web Developer",
+      startDate: "2019/12",
+      endDate: "2019/09",
+      mainDetails:
+        "As a full stack developer, my goal at Okhati was to create pixel perfect web components, design architecture of native applications and implement back-end logic for them to work bug-free with tested,documented code that's scalable in future",
+      details: [
+        "Keep the code quality high reviewing code from other developersand suggesting improvements",
+        "Interact with the designer to suggest changes and to make sure the view he has about design in translated into actual functionality",
+      ],
+    },
+  ],
+  education: [
+    {
+      name: "Kantipur City College",
+      startDate: "2015",
+      endDate: "2019",
+      subject: "Bachelors in Information Technology",
+    },
+    {
+      name: "Everest International College",
+      startDate: "2012",
+      endDate: "2014",
+      subject: "+2",
+    },
+    {
+      name: "Hill Town School",
+      startDate: "2002",
+      endDate: "2012",
+      subject: "High School",
+    },
+  ],
+};
